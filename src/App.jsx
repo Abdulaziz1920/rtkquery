37df65f707646b2b4541110f42b1575a71eead59
Abdulaziz1920 <abdulazizgit1920@gmail.com>
2023-08-02 20:43:09 +0500

    error finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import Todos from "./pages/todos";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Todos />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Todos />;
 }
 
 export default App;
